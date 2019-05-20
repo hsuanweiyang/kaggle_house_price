dce11c913851f6641bbf2d7c42cd0f699e8f334d
Hsuan Wei Yang <e24006747@gmail.com>
2019-05-20 16:54:09 +0800

    new fully connected model, and svr, xgboost, rf predictor

---
+++ model_fc.py
@@ -0,0 +1,173 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sys import argv
+from xgboost import XGBRegressor
+import math
+
+
+def read_input_data(input_file):
+    input_data = pd.read_pickle(input_file)
+    y = input_data['SalePrice']
+    x = pd.DataFrame.transpose(input_data.iloc[:, 1:])
+    return x, y
+
+
+def train_valid_split(input_x, input_y, train_portion=0.9):
+    sample_num = input_x.shape[1]
+    train_num = int(sample_num * train_portion)
+    permutation = list(np.random.permutation(sample_num))
+    shuffled_x = input_x.iloc[:, permutation]
+    shuffled_y = input_y[permutation]
+    train_x = shuffled_x.iloc[:, :train_num]
+    valid_x = shuffled_x.iloc[:, train_num:]
+    train_y = shuffled_y[:train_num]
+    valid_y = shuffled_y[train_num:]
+    return train_x, train_y, valid_x, valid_y
+
+
+def create_placeholders(n_f):
+    X = tf.placeholder(tf.float32, [n_f, None])
+    Y = tf.placeholder(tf.float32, [None])
+    return X, Y
+
+
+def initialize_parameters(n_f):
+    w1 = tf.get_variable('w1', [80, n_f], initializer=tf.keras.initializers.he_normal())
+    b1 = tf.get_variable('b1', [80, 1], initializer=tf.zeros_initializer())
+    w2 = tf.get_variable('w2', [60, 80], initializer=tf.keras.initializers.he_normal())
+    b2 = tf.get_variable('b2', [60, 1], initializer=tf.zeros_initializer())
+    w3 = tf.get_variable('w3', [40, 60], initializer=tf.keras.initializers.he_normal())
+    b3 = tf.get_variable('b3', [40, 1], initializer=tf.zeros_initializer())
+    w4 = tf.get_variable('w4', [1, 40], initializer=tf.keras.initializers.he_normal())
+    b4 = tf.get_variable('b4', [1, 1], initializer=tf.zeros_initializer())
+    parameters = {'w1': w1, 'b1': b1,
+                  'w2': w2, 'b2': b2,
+                  'w3': w3, 'b3': b3,
+                  'w4': w4, 'b4': b4}
+    return parameters
+
+
+def add_layer(W, b, X, activation=tf.nn.relu):
+    Z = tf.matmul(W, X) + b
+    A = activation(Z)
+    return A
+
+
+def forward_propogation(X, parameters, drop_rate):
+    w1 = parameters['w1']
+    b1 = parameters['b1']
+    w2 = parameters['w2']
+    b2 = parameters['b2']
+    w3 = parameters['w3']
+    b3 = parameters['b3']
+    w4 = parameters['w4']
+    b4 = parameters['b4']
+
+    A1 = add_layer(w1, b1, X)
+    A1 = tf.nn.dropout(A1, rate=drop_rate)
+    A2 = add_layer(w2, b2, A1)
+    A2 = tf.nn.dropout(A2, rate=drop_rate)
+    A3 = add_layer(w3, b3, A2)
+    Z_out = tf.matmul(w4, A3) + b4
+    #Z_xg = XGBRegressor()
+    return Z_out
+
+
+def compute_cost(Y, Z_out):
+    Y = tf.log(tf.clip_by_value(Y, 1e-8, math.inf))
+    Z_out = tf.log(tf.clip_by_value(Z_out, 1e-8, math.inf))
+    loss = tf.math.sqrt(tf.losses.mean_squared_error(Y, tf.squeeze(Z_out)))
+    return loss
+
+
+def log_cost(Y, Z_out):
+    Y = tf.log(Y)
+    Z_out = tf.log(Z_out)
+    loss = tf.math.sqrt(tf.losses.mean_squared_error(Y, tf.squeeze(Z_out)))
+    return loss
+
+
+def random_minibatch(x, y, minibatch_size=64, seed=0):
+    n_sample = x.shape[1]
+    mini_batches = []
+    np.random.seed(seed)
+
+    permutation = np.random.permutation(n_sample)
+    shuffled_x = x.iloc[:, permutation]
+    shuffled_y = y.iloc[permutation]
+
+    num_batches = n_sample//minibatch_size
+
+    for i in range(num_batches):
+        mini_x = shuffled_x.iloc[:, i * minibatch_size:(i+1) * minibatch_size]
+        mini_y = shuffled_y.iloc[i * minibatch_size:(i+1) * minibatch_size]
+        mini_batches.append((mini_x, mini_y))
+
+    if n_sample&minibatch_size != 0:
+        mini_x = shuffled_x.iloc[:, num_batches * minibatch_size:]
+        mini_y = shuffled_y.iloc[num_batches * minibatch_size:]
+        mini_batches.append((mini_x, mini_y))
+
+    return mini_batches
+
+
+def model(train_x, train_y, valid_x, valid_y, learing_rate=0.001, num_epoch=10000, minibatch_size=128, dropout_rate=0.,
+          print_cost=True, draw=True):
+    n_feature = train_x.shape[0]
+    costs = []
+    valid_costs = []
+
+    # Train
+    X, Y = create_placeholders(n_feature)
+    parameters = initialize_parameters(n_feature)
+    drop_rate = tf.placeholder(tf.float32, name='drop_rate')
+
+    Z_out = forward_propogation(X, parameters, drop_rate)
+    log_loss = log_cost(Y, Z_out)
+    loss = compute_cost(Y, Z_out)
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learing_rate).minimize(loss)
+    saver = tf.train.Saver(max_to_keep=5)
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        for epoch in range(num_epoch):
+            epoch_loss = 0.
+            minibatces = random_minibatch(train_x, train_y, minibatch_size)
+            num_minibatches = len(minibatces)
+
+            for mini_batch in minibatces:
+                (mini_x, mini_y) = mini_batch
+                _, mini_loss = sess.run([optimizer, loss], feed_dict={X: mini_x, Y: mini_y, drop_rate: dropout_rate})
+                epoch_loss += mini_loss/num_minibatches
+            if (epoch+1)%100 == 0 and print_cost:
+                print('epoch-{0}: {1}'.format(epoch+1, epoch_loss))
+                saver.save(sess, 'model_fc_e-{0}_lr-{1}_d-{2}/model_fc'.format(num_epoch, learing_rate, dropout_rate),
+                           global_step=epoch+1)
+        print('Train RMSE:', log_loss.eval({X: train_x, Y: train_y, drop_rate: 0.}))
+        print('Valid RMSE:', log_loss.eval({X: valid_x, Y: valid_y, drop_rate: 0.}))
+        predict = Z_out.eval({X:valid_x, drop_rate:0.})
+        for i in range(10):
+            print(valid_y.iloc[i], '\t', predict[0, i])
+        parameters = sess.run(parameters)
+    return parameters
+
+
+
+if __name__ == '__main__':
+    opt = argv[1]
+    input_file = argv[2]
+    if opt == '-tr':
+        train_opt = argv[3:]
+        epoch_num = 1000
+        batch_size = 64
+        learning_rate = 0.001
+
+
+
+    features, label = read_input_data(input_file)
+    X_train, Y_train, X_valid, Y_valid = train_valid_split(features, label)
+    model(X_train, Y_train, X_valid, Y_valid, dropout_rate=0.)
---
+++ predictor.py
@@ -0,0 +1,56 @@
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+from sys import argv
+import numpy as np
+from xgboost import XGBRegressor
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+input_train_file = argv[1]
+input_test_file = argv[2]
+reg_opt = argv[3]
+numerical_features_idx = ['LotArea', 'LotFrontage', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea',
+                      'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
+                      'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr',
+                      'TotRmsAbvGrd', 'Fireplaces', 'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
+                      'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold']
+
+def norm(data):
+    data[numerical_features_idx] = (data[numerical_features_idx]-data[numerical_features_idx].mean())/\
+                                   (data[numerical_features_idx].max()-data[numerical_features_idx].min())
+    return data
+
+
+input_train_data = pd.read_pickle(input_train_file)
+input_test_data = pd.read_pickle(input_test_file)
+train_x = input_train_data.iloc[:1200, 1:]
+#train_x = norm(train_x)
+train_y = input_train_data.iloc[:1200, 0]
+valid_x = input_train_data.iloc[1200:, 1:]
+#valid_x = norm(valid_x)
+valid_y = input_train_data.iloc[1200:, 0]
+
+
+
+if reg_opt == 'xg':
+    reg = XGBRegressor(learning_rate=0.07, max_depth=4, n_estimators=300, min_child_weight=1, gamma=0.5)
+elif reg_opt == 'svr':
+    reg = SVR()
+elif reg_opt == 'rf':
+    reg = RandomForestRegressor()
+reg.fit(train_x, train_y)
+
+valid_result =reg.predict(valid_x)
+
+rmse = np.sqrt(np.mean(np.square(np.log(valid_result)-np.log(valid_y))))
+print(rmse)
+
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#    print(input_test_data)
+predict_result = reg.predict(input_test_data)
+
+with open('submit_{0}.csv'.format(reg_opt), 'w+') as output_file:
+    output_file.write('Id,SalePrice\n')
+    for i in range(1461, 2920):
+        output_file.write('{0},{1}\n'.format(i, predict_result[i-1461]))
