dea3f6391ff1b4aebcaa824e9857d5e104e54a33
Hsuan Wei Yang <e24006747@gmail.com>
2019-05-27 01:06:21 +0800

    remove batch normalization add drop out and l2 regularization

--- model_fc.py
+++ model_fc.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sys import argv
 from xgboost import XGBRegressor
 import math
+from sklearn.ensemble import RandomForestRegressor
 
 
 def read_input_data(input_file):
@@ -33,14 +34,14 @@ def create_placeholders(n_f):
 
 
 def initialize_parameters(n_f):
-    w1 = tf.get_variable('w1', [80, n_f], initializer=tf.keras.initializers.he_normal())
-    b1 = tf.get_variable('b1', [80, 1], initializer=tf.zeros_initializer())
-    w2 = tf.get_variable('w2', [60, 80], initializer=tf.keras.initializers.he_normal())
+    w1 = tf.get_variable('w1', [40, n_f], initializer=tf.keras.initializers.he_normal())
+    b1 = tf.get_variable('b1', [40, 1], initializer=tf.zeros_initializer())
+    w2 = tf.get_variable('w2', [60, 40], initializer=tf.keras.initializers.he_normal())
     b2 = tf.get_variable('b2', [60, 1], initializer=tf.zeros_initializer())
-    w3 = tf.get_variable('w3', [40, 60], initializer=tf.keras.initializers.he_normal())
-    b3 = tf.get_variable('b3', [40, 1], initializer=tf.zeros_initializer())
-    w4 = tf.get_variable('w4', [1, 40], initializer=tf.keras.initializers.he_normal())
-    b4 = tf.get_variable('b4', [1, 1], initializer=tf.zeros_initializer())
+    w3 = tf.get_variable('w3', [30, 60], initializer=tf.keras.initializers.he_normal())
+    b3 = tf.get_variable('b3', [30, 1], initializer=tf.zeros_initializer())
+    w4 = tf.get_variable('w4', [20, 30], initializer=tf.keras.initializers.he_normal())
+    b4 = tf.get_variable('b4', [20, 1], initializer=tf.zeros_initializer())
     parameters = {'w1': w1, 'b1': b1,
                   'w2': w2, 'b2': b2,
                   'w3': w3, 'b3': b3,
@@ -48,7 +49,7 @@ def initialize_parameters(n_f):
     return parameters
 
 
-def add_layer(W, b, X, activation=tf.nn.relu):
+def add_layer(W, b, X, activation=tf.nn.leaky_relu):
     Z = tf.matmul(W, X) + b
     A = activation(Z)
     return A
@@ -69,14 +70,16 @@ def forward_propogation(X, parameters, drop_rate):
     A2 = add_layer(w2, b2, A1)
     A2 = tf.nn.dropout(A2, rate=drop_rate)
     A3 = add_layer(w3, b3, A2)
+    A3 = tf.nn.dropout(A3, rate=drop_rate)
+    #A4 = add_layer(w4, b4, A3)
+    #A4 = tf.nn.dropout(A4, rate=drop_rate)
     Z_out = tf.matmul(w4, A3) + b4
-    #Z_xg = XGBRegressor()
     return Z_out
 
 
 def compute_cost(Y, Z_out):
-    Y = tf.log(tf.clip_by_value(Y, 1e-8, math.inf))
-    Z_out = tf.log(tf.clip_by_value(Z_out, 1e-8, math.inf))
+    #Y = tf.log(Y)
+    #Z_out = tf.log(tf.clip_by_value(Z_out, 1e-8, math.inf)) # clip the value to prevent predicting negative num
     loss = tf.math.sqrt(tf.losses.mean_squared_error(Y, tf.squeeze(Z_out)))
     return loss
 
@@ -113,7 +116,7 @@ def random_minibatch(x, y, minibatch_size=64, seed=0):
 
 
 def model(train_x, train_y, valid_x, valid_y, learing_rate=0.001, num_epoch=10000, minibatch_size=128, dropout_rate=0.,
-          print_cost=True, draw=True):
+          regularization = 0., print_cost=True, draw=True):
     n_feature = train_x.shape[0]
     costs = []
     valid_costs = []
@@ -126,6 +129,11 @@ def model(train_x, train_y, valid_x, valid_y, learing_rate=0.001, num_epoch=1000
     Z_out = forward_propogation(X, parameters, drop_rate)
     log_loss = log_cost(Y, Z_out)
     loss = compute_cost(Y, Z_out)
+    if regularization > 0.:
+        reg_loss = 0
+        for key in ['w1', 'w2', 'w3', 'w4']:
+            reg_loss += tf.reduce_sum(tf.square(parameters[key]))
+        loss += reg_loss
     with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
         optimizer = tf.train.AdamOptimizer(learning_rate=learing_rate).minimize(loss)
     saver = tf.train.Saver(max_to_keep=5)
@@ -145,11 +153,13 @@ def model(train_x, train_y, valid_x, valid_y, learing_rate=0.001, num_epoch=1000
                 epoch_loss += mini_loss/num_minibatches
             if (epoch+1)%100 == 0 and print_cost:
                 print('epoch-{0}: {1}'.format(epoch+1, epoch_loss))
+                print('Train:', log_loss.eval({X: train_x, Y: train_y, drop_rate: 0.}))
+                print('Valid:', log_loss.eval({X: valid_x, Y: valid_y, drop_rate: 0.}))
                 saver.save(sess, 'model_fc_e-{0}_lr-{1}_d-{2}/model_fc'.format(num_epoch, learing_rate, dropout_rate),
                            global_step=epoch+1)
         print('Train RMSE:', log_loss.eval({X: train_x, Y: train_y, drop_rate: 0.}))
         print('Valid RMSE:', log_loss.eval({X: valid_x, Y: valid_y, drop_rate: 0.}))
-        predict = Z_out.eval({X:valid_x, drop_rate:0.})
+        predict = Z_out.eval({X:valid_x, Y:valid_y, drop_rate:0.})
         for i in range(10):
             print(valid_y.iloc[i], '\t', predict[0, i])
         parameters = sess.run(parameters)
@@ -162,12 +172,13 @@ if __name__ == '__main__':
     input_file = argv[2]
     if opt == '-tr':
         train_opt = argv[3:]
-        epoch_num = 1000
+        epoch_num = 10000
         batch_size = 64
         learning_rate = 0.001
+        regularization = 0.06
 
 
 
     features, label = read_input_data(input_file)
     X_train, Y_train, X_valid, Y_valid = train_valid_split(features, label)
-    model(X_train, Y_train, X_valid, Y_valid, dropout_rate=0.)
+    model(X_train, Y_train, X_valid, Y_valid, num_epoch=epoch_num, dropout_rate=0., regularization=regularization)
